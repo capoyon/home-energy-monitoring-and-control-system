5c6360273adb1207b1f1d81e13b82ee5aaa2a095
Cristopher <email.cristopher@gmail.com>
2024-04-14 12:32:17 +0800

    fix mobile view style

--- html/general.js
+++ html/general.js
@@ -28,3 +28,19 @@ observer.observe(content, { attributes: true, childList: true, subtree: true });
 
 // Initial call to adjustContentHeight
 adjustContentHeight();
+
+// update time
+function updateTime(epochTime, use24HourFormat = false) {
+    const date = new Date(epochTime * 1000);
+    let options = { weekday: 'short', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+
+    if (use24HourFormat) {
+        options.hour12 = false;
+    }
+
+    const timeString = date.toLocaleString('en-US', options).replace(/,/g, '');
+    document.getElementById('time').innerText = timeString;
+}
+
+updateTime(Date.now() / 1000);
+setInterval(() => updateTime(Date.now() / 1000), 1000);
