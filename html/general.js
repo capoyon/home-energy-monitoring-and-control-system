2502da3aed6b06be0243a5643f6507d3594fdda7
Cristopher <email.cristopher@gmail.com>
2024-04-14 12:50:30 +0800

    fix mobile view style

--- html/general.js
+++ html/general.js
@@ -1,33 +1,20 @@
-function adjustContentHeight() {
-    var element = document.getElementById('content');
-    var deviceHeight = window.innerHeight;
-    var elementHeight = element.offsetHeight;
-    if (elementHeight < deviceHeight) {
-        element.style.height = deviceHeight - 90 + 'px';
+function adjustContentHeight(elementHeight) {
+    if(elementHeight>0){
+        element.style.height = elementHeight + 'px';
     } else {
-        element.style.height = ''; // Remove the height attribute
-    }
-}
-
-// Function to handle mutations in the content div or its subtree
-function handleMutation(mutationsList, observer) {
-    for(var mutation of mutationsList) {
-        if (mutation.type === 'attributes' || mutation.type === 'childList') {
-            adjustContentHeight();
-            break;
+        var element = document.getElementById('content');
+        var deviceHeight = window.innerHeight;
+        var elementHeight = element.offsetHeight;
+        if (elementHeight < deviceHeight) {
+            element.style.height = deviceHeight - 90 + 'px';
+        } else {
+            element.style.height = elementHeight + 'px';
         }
     }
 }
-
-// Create a MutationObserver instance
-var observer = new MutationObserver(handleMutation);
-
-// Start observing the content div and its subtree
-var content = document.getElementById('content');
-observer.observe(content, { attributes: true, childList: true, subtree: true });
-
-// Initial call to adjustContentHeight
+window.addEventListener('resize', adjustContentHeight);
 adjustContentHeight();
+
 
 // update time
 function updateTime(epochTime, use24HourFormat = false) {
